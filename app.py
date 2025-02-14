3f2bc1e5ac8154ec14ab35485530200bb5020109
ThomasPLS <playstorethomasplesse@gmail.com>
2025-02-14 13:42:35 +0100

    verification utilisateur

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://asphalt_records:i2XTTixzUFAX3CIdtTPccJ9sRdcFEoqH@dpg-cun9i4tsvqrc7390jcug-a/asphaltrecords_db')
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -36,6 +37,35 @@ def login():
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to my API!"})
+
+# Connexion à la base de données
+conn = psycopg2.connect(
+    dbname="votre_dbname",
+    user="votre_user",
+    password="votre_password",
+    host="votre_host",
+    port="votre_port"
+)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = [{"id": user.id, "username": user.username} for user in users]
+    return jsonify(users_list)
+
+
+
+# Création d'un curseur pour exécuter des requêtes
+cur = conn.cursor()
+
+# Exécution d'une requête SQL
+cur.execute("SELECT version();")
+db_version = cur.fetchone()
+print("Version de PostgreSQL :", db_version)
+
+# Fermeture de la connexion
+cur.close()
+conn.close()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Utilise le port de Render ou 5000 en local
